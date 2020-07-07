51074c287d3f96f1ce5ed47051191db1372981a5
venkatrag1 <venkatraghavan.r@gmail.com>
2020-07-07 10:46:35 +0000

    setup actions and reducers

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from '../utils/api';
+import { receiveTweets } from '../actions/tweets';
+import { receiveUsers } from '../actions/users';
+import { setAuthedUser } from '../actions/authedUser';
+
+const AUTHED_ID - 'tylermcginnis'
+
+export function handleInitialData() {
+    return (dispatch) => {
+        getInitialData.then(({tweets, users}) =>
+            dispatch(receiveTweets(tweets));
+            dispatch(receiveUsers(users));
+            dispatch(setAuthedUser(AUTHED_ID))
+        );
+    };
+}
