d9efb5b194f1bc7e8a0a424b2c0f23fad10d9491
venkatrag1 <venkatraghavan.r@gmail.com>
2020-07-12 23:03:06 +0000

    parent tweet + react-icons

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
- import { connect } from 'react-redux';
- import { formatTweet } from '../utils/helpers';
+import { connect } from 'react-redux';
+import { formatTweet } from '../utils/helpers';
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from "react-icons/ti";
 
 class Tweet extends Component {
     render() {
+        const { tweet } = this.props;
+        if (tweet === null) {
+            return <p>This tweet doesn't exist</p>
+        }
+
         console.log(this.props);
         return (
             <div className='tweet'></div>
         );
-
     }
 
 }
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
     const tweet = tweets[id];
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null
     };
 }
 
